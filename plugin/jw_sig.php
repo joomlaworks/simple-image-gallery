96d77b159ea1e61fe7cf5197f308f23324c75753
thex <andreas.kar@gmx.at>
2020-01-25 14:01:01 +0100

    option to disable mootools

--- plugin/jw_sig.php
+++ plugin/jw_sig.php
@@ -156,6 +156,7 @@ class plgContentJw_sig extends JPlugin
 
         $galleries_rootfolder = ($params->get('galleries_rootfolder')) ? $params->get('galleries_rootfolder') : $pluginParams->get('galleries_rootfolder', $defaultImagePath);
         $popup_engine = 'jquery_fancybox';
+        $mootools = $pluginParams->get('mootools', 1);
         $jQueryHandling = $pluginParams->get('jQueryHandling', '1.12.4');
         $thb_template = 'Classic';
         $thb_width = (!is_null($params->get('thb_width', null))) ? $params->get('thb_width') : $pluginParams->get('thb_width', 200);
@@ -254,12 +255,14 @@ class plgContentJw_sig extends JPlugin
                         require $popupRequire;
                     }
 
-                    if (version_compare(JVERSION, '4', 'ge')) {
-                        // Do nothing
-                    } elseif (version_compare(JVERSION, '2.5.0', 'ge')) {
-                        JHtml::_('behavior.framework');
-                    } else {
-                        JHTML::_('behavior.mootools');
+                    if($mootools) {
+                        if (version_compare(JVERSION, '4', 'ge')) {
+                            // Do nothing
+                        } elseif (version_compare(JVERSION, '2.5.0', 'ge')) {
+                            JHtml::_('behavior.framework');
+                        } else {
+                            JHTML::_('behavior.mootools');
+                        }
                     }
 
                     if (count($stylesheets)) {
